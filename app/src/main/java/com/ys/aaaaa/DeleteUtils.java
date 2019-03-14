62f4cf75e6f81ef8006df12c2ed8044578be05d5
wangyun <Wangyun@0528>
2019-03-14 16:54:41 +0800

    hehehe

--- app/src/main/java/com/ys/aaaaa/DeleteUtils.java
+++ app/src/main/java/com/ys/aaaaa/DeleteUtils.java
@@ -2,10 +2,8 @@ package com.ys.aaaaa;
 
 import android.util.Log;
 
-import java.security.acl.LastOwnerException;
-
 public class DeleteUtils {
     private void delete() {
-        Log.e("TAG", "AAA")
+        Log.e("TAG", "AAA");
     }
 }
